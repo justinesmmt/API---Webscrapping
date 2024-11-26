66a151d623e6418adc9be07a9380b2374443221e
justinesmmt <justine.sammut@epfedu.fr>
2024-11-26 16:07:10 +0100

    step 3 TP2 done

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,14 @@
 import uvicorn
-
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from src.app import get_application
 
 app = get_application()
+
+# Step 3: Redirect root ("/") to the Swagger UI automatically
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
