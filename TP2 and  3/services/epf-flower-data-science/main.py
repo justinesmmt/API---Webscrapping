11245c72ecbbcc56bde924ab53b44c5e8d62da52
justinesmmt <justine.sammut@epfedu.fr>
2024-12-03 10:07:34 +0100

    step 4

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from src.app import get_application
+from src.api.routes.data import router as data_router
 
 app = get_application()
 
@@ -9,6 +10,8 @@ app = get_application()
 @app.get("/")
 async def redirect_to_docs():
     return RedirectResponse(url="/docs")
+
+app.include_router(data_router) 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
