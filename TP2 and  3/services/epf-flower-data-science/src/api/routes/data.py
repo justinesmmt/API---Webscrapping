438c4c003d07b88aeed90c672e2a343fe4b40b30
justinesmmt <justine.sammut@epfedu.fr>
2024-12-03 12:49:49 +0100

    preprocessed data

--- TP2 and  3/services/epf-flower-data-science/src/api/routes/data.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/routes/data.py
@@ -3,6 +3,11 @@ import opendatasets as od
 from fastapi import APIRouter, HTTPException
 import os
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from typing import List
+from typing import Dict, Any
 
 router = APIRouter()
 
@@ -25,3 +30,39 @@ async def get_iris_data():
         raise HTTPException(status_code=404, detail="Dataset not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# Step 8: Processing the Iris dataset (encoding, scaling, etc.)
+@router.get("/process-iris-dataset", response_model=Dict[str, Any])
+async def process_iris_data():
+    try:
+        dataset_path = "src/data/iris/Iris.csv"
+        
+        # Load the dataset
+        df = pd.read_csv(dataset_path)
+
+        # Drop nan
+        df = df.dropna()
+
+        # Drp column Id
+        df.drop(columns=["Id"])
+
+        # Label Encoding the 'species' column
+        encoder = LabelEncoder()
+        df['Species'] = encoder.fit_transform(df['Species'])  # Encoding categorical values to numbers
+
+        # Save the csv
+        df.to_csv("src/data/iris/Iris_preprocessed.csv", index=False)
+
+        # Debugging: Print the cleaned dataset
+        print("Cleaned Data:")
+        print(df.head())
+
+        # Return a success message and some data
+        return {"message": "Dataset processed successfully", "processed_data": df.to_dict(orient="records")}
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Dataset not found")
+    except Exception as e:
+        # Log the error message for debugging purposes
+        print(f"Error processing dataset: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
